3313cba326b94bdbd1303adff841d2a46f0b87cc
Pranav Raikote <43779710+pranavraikote@users.noreply.github.com>
2019-06-27 19:24:44 +0530

    Add files via upload

---
+++ Certificate_Automation.py
@@ -0,0 +1,44 @@
+from PIL import Image, ImageDraw, ImageFont
+import time
+import os
+
+#f = open('New Text document.txt','r')
+#names = f.read().splitlines()
+
+names=['Put in names here']
+
+#a=os.getcwd()
+#folder_name = '6th Sem'
+#if not os.path.exists(folder_name):
+#    os.mkdir(folder_name)
+#    a=a+'\\'+folder_name
+#    os.chdir(a)
+
+
+for i in names:    
+
+    image = Image.open('Certificate.png')
+
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype('Ubuntu-Regular.ttf', size=32)
+
+    (x,y) = (390,329)
+
+    message = i
+
+    color = 'rgb(0,0,0)'
+
+    draw.text((x,y), message, fill=color, font=font)
+
+    message1 = 'II'
+
+    (x1,y1) = (754,329)
+
+    save = message+'.png'
+
+    draw.text((x1,y1), message1, fill=color, font=font)
+
+    image.save(save)
+
+
